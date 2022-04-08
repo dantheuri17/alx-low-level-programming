3e688a757e6197cc158748d7a06f78599f400b42
Daniel Theuri <dantheuri@gmail.com>
2022-04-08 14:48:41 -0700

    it

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - prints letters of alphabet
+ * uses putchar twice
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}	
