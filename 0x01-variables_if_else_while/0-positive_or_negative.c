56095484063e84d996fb35dbd41f4ba7942ccbb0
Daniel Theuri <dantheuri@gmail.com>
2022-04-08 06:56:14 -0700

    positiven ow

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,10 @@
 #include <time.h>
 #include<stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main- assigns random number to n
+ * prints whether number is negative, positive, or zero
+ * */
 int main(void)
 {
 	int n;
