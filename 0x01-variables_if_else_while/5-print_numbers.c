8200eab0d4f36797d9a03067ad11167a015ba865
Daniel Theuri <dantheuri@gmail.com>
2022-04-08 15:41:09 -0700

    hello

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - printing digits of base 10
+ * then enter
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i; 
+
+	for(i = 1; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
