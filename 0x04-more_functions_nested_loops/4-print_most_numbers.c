7354a94ea7d8fc4234b3765fd6a7f38da86e80dc
Daniel Theuri <dantheuri@gmail.com>
2022-04-18 08:37:58 -0700

    smile

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print everything but 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i != '2' || i!= '4')
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
