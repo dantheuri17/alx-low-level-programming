09c4843072725a7ef9925cd1f5a33f5b54274292
Daniel Theuri <dantheuri@gmail.com>
2022-04-21 16:59:25 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts - prints a string
@@ -10,5 +9,12 @@
 
 void _puts(char *str)
 {
-	puts(str);
+	int i = 0;
+
+	while (i != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
