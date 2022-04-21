62db1c91214ba8d5b591ba584f03fdaf13450d4e
Daniel Theuri <dantheuri@gmail.com>
2022-04-21 16:33:44 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,13 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < n - 1)
+	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if(i < n - 1)
+			printf(", ");
+
 		i++;
 	}
-	printf("%d", a[n-1]);
 	_putchar('\n');
 }
