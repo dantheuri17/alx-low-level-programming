574d69c3dcaef9d8452c8c4ca0aaf6c5e90e003f
Daniel Theuri <dantheuri@gmail.com>
2022-04-24 05:44:59 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/** 
+ * _strncat - concatenate 2 strings with certain number of 
+ * @src: string concatenating
+ * @dest: string being concatenated
+ * @n: number of values 
+ *
+ * Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dlen = 0, i; 
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+
+	return (dest);
+}
